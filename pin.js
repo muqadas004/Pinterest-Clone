a60d3123d6f9ca0a3bc0b8ee7c87e53743c7e6e9
muqadas004 <160884836+muqadas004@users.noreply.github.com>
2024-09-05 21:22:52 +0500

    Add files via upload
    #html #css #js #interest-clone #array #query-selector

---
+++ pin.js
@@ -0,0 +1,163 @@
+var data = [
+    {
+        "id": 1,
+        "target": "boredpanda.com",
+        "imageUrl": "https://i.pinimg.com/236x/23/6e/5a/236e5af3426b5e01ac1e6948712721a7.jpg",
+        "url": "https://www.boredpanda.com/summer-heatwave-high-temperature-photos/?utm_source=pinterest35&utm_medium=social&utm_campaign=jamal",
+        "title": "50 Raw And Funny Pics From People Who Were Just Trying To Get Through A Summer Heatwave (New Pics)",
+        "description": "If all of the heatwaves and forest fires haven’t yet made it clear—the summer this year was an extreme one... just like the ones before it. And unless you’ve been living under an especially cool rock, you probably spent a lot of time making sure you're drinking enough water, lounging in the shade, and praying for cooler weather.",
+        "user": {
+            "username": "girls Fashion",
+            "profileImage": "https://i.pinimg.com/280x280_RS/8a/e2/a3/8ae2a3aafd0ae4ce5b957eff76cf7ed8.jpg",
+            "followers": "200 followers"
+        }
+    },
+    {
+        "id": 2,
+        "target": "thegoodtrade.com",
+        "imageUrl": "https://i.pinimg.com/564x/d5/07/cc/d507ccb186b5e422bfd389c9144d2d7b.jpg",
+        "url": "https://www.thegoodtrade.com/features/vintage-record-player-how-to-shop",
+        "title": "Your Guide to find a vintage Record Player",
+        "description": "If you prefer vinyl over streaming, here’s our how-to for finding a vintage record player and sourcing the best vinyl.",
+        "user": {
+            "username": "The Good Trade",
+            "profileImage": "https://i.pinimg.com/75x75_RS/a0/33/64/a03364965004c70dcaadedb6fba37871.jpg",
+            "followers": "57.4k followers"
+        }
+    },
+    {
+        "id": 3,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/736x/aa/1f/ee/aa1fee1544bbe0c4b652a32fe7ee9b5b.jpg",
+        "url": "",
+        "title": "Urban Elegance: Sony A7R5 Captures Porsche GT2 RS in Citycape with AI Brilliancce",
+        "description": "In a visual journey, my Sony A7R5 lens frames the Porsche GT2 RS against the dynamic backdrop of city streets. These street photography wallpapers transcend the ordinary, seamlessly merging the powerful allure of the GT2 RS with the vibrant energy of urban landscapes. Guided by the brilliant touch of AI and Midjourney, each image becomes a unique fusion of automotive excellence and the pulse of city life. This collection invites viewers to explore beyond traditional photography boundaries, where cutting-edge technology harmonizes with the ever-changing rhythm of city streets.",
+        "user": {
+            "username": "lehari",
+            "profileImage": "https://i.pinimg.com/75x75_RS/59/05/8e/59058ef89712ccd66bbe6428080e15c4.jpg",
+            "followers": "6.3k followers"
+        }
+    },
+    {
+        "id": 4,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/736x/da/e9/1b/dae91be07cf3b866ac21a076554cbb92.jpg",
+        "url": "",
+        "title": "",
+        "description": "🌟 Share Your Ideas with Us! 🌟😍Comment Now My Friend😍",
+        "user": {
+            "username": "vast",
+            "profileImage": "https://i.pinimg.com/75x75_RS/56/b3/cb/56b3cb9cee89fb7a68350496728ad439.jpg",
+            "followers": "716 followers"
+        }
+    },
+    {
+        "id": 5,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/564x/df/5f/68/df5f68694d16b422cefc246c41c5e57d.jpg",
+        "url": "",
+        "title": "",
+        "description": "",
+        "user": {
+            "username": "Edgar Antonio",
+            "profileImage": "https://i.pinimg.com/75x75_RS/10/d8/e9/10d8e98b515caca9c8fad56a0693fe16.jpg",
+            "followers": "12k followers"
+        }
+    },
+    {
+        "id": 6,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/736x/05/60/1e/05601ed72fdfa3955bf812fc48e8d8c7.jpg",
+        "url": "",
+        "title": "",
+        "description": "Manhattan, NYC",
+        "user": {
+            "username": "Hailey",
+            "profileImage": "https://i.pinimg.com/75x75_RS/95/b2/8d/95b28d0a9cfd41720338931e2f95a127.jpg",
+            "followers": "51 followers"
+        }
+    },
+    {
+        "id": 7,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/564x/4f/b9/aa/4fb9aab9e97f2f04d3045d9fa7b17482.jpg",
+        "url": "",
+        "title": "",
+        "description": "",
+        "user": {
+            "username": "Kevin Sanchez",
+            "profileImage": "https://i.pinimg.com/75x75_RS/95/b2/8d/95b28d0a9cfd41720338931e2f95a127.jpg",
+            "followers": "167 followers"
+        }
+    },
+    {
+        "id": 8,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/736x/ff/d5/f8/ffd5f879a895c3f7bcaf75a073da7fab.jpg",
+        "url": "",
+        "title": "PRODUCT NT'S Wonderland",
+        "description": "! Welcome to NT's Wonderland.🎉 It's really nice to see you here. 🌟You're one step closer to changing the lives of a stranger and fulfilling their aspirations! 💜NT's Wonderland is a genuine tiny firm with lofty aspirations and unstoppable objectives.",
+        "user": {
+            "username": "NT'S Wonderland",
+            "profileImage": "https://i.pinimg.com/75x75_RS/ab/d8/cb/abd8cb2e5ff78ddb22400cbd757b5e43.jpg",
+            "followers": "1.9k followers"
+        }
+    },
+    {
+        "id": 9,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/564x/25/a4/2e/25a42efe91fa122386e1044cd5d2aef2.jpg",
+        "url": "",
+        "title": "",
+        "description": "",
+        "user": {
+            "username": "Joe Hyatt Art",
+            "profileImage": "https://i.pinimg.com/75x75_RS/c4/82/1a/c4821a19f94ef2f5a9637b96a1ae40a8.jpg",
+            "followers": "374 followers"
+        }
+    },
+    {
+        "id": 10,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/736x/0d/25/f9/0d25f9ba6f45b81c9874151a72b9a228.jpg",
+        "url": "",
+        "title": "",
+        "description": "Harry Potter",
+        "user": {
+            "username": "Efren rodriguez",
+            "profileImage": "https://i.pinimg.com/75x75_RS/61/65/6d/61656d4acd60d08d48b062010feb88d2.jpg",
+            "followers": "189 followers"
+        }
+    },
+    {
+        "id": 11,
+        "target": "www.pinterest.com",
+        "imageUrl": "https://i.pinimg.com/564x/a3/ba/38/a3ba387a7ead38492809aeb23f970900.jpg",
+        "url": "https://www.pinterest.com/pin/882494489471746170",
+        "title": "it's a beautiful world",
+        "description": "i also known as the Beaver State, has some of the most beautiful and spectacular sights and places to visit! Just browse through these awesome pictures I found and be...",
+        "user": {
+            "username": "fiza",
+            "profileImage": "https://i.pinimg.com/75x75_RS/cf/30/e2/cf30e2e5718cc09f73470b5f93d26efe.jpg",
+            "followers": "3k followers"
+        }
+    },
+    {
+        "id": 12,
+        "target": "",
+        "imageUrl": "https://i.pinimg.com/564x/37/fe/0f/37fe0f43cd3887b93d200a1e93a52883.jpg",
+        "url": "",
+        "title": "✪⁀➷AI Art",
+        "description": "⁀➷ Wallpaper ✪",
+        "user": {
+            "username": "ᨐᵢₖᵤ ⑅.˚",
+            "profileImage": "https://i.pinimg.com/75x75_RS/1e/f1/10/1ef1102d91d2b4bb4540fafa4e687378.jpg",
+            "followers": "14.8k followers"
+        }
+    },
+];
+
+function goToPage(){
+    window.location.href = "pin.html";
+}
+
